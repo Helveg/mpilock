14937d6b0efb797bde7ac479259650456b1ca6fa
Robin De Schepper <robin.deschepper93gmail.com>
2021-03-16 19:25:28 +0100

    mock numpy module for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,4 +60,4 @@ html_theme = "furo"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-autodoc_mock_imports = ["mpi4py"]
+autodoc_mock_imports = ["mpi4py", "numpy"]
